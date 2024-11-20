6f14f687360edba0f3a49f03bdc0738f1e42332f
dosabhijith <dosabhijith@gmail.com>
2024-11-20 18:40:26 +0530

    create: create header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import "./Header.css"
+
+
+const Header = () => {
+
+    return (
+
+        <header>
+            <div className='header'>
+                <div className='header-logo'>
+                    <img src="https://optimal-demos.myshopify.com/cdn/shop/files/dm2-logo.png?v=1632039937" alt="header-logo" />
+                </div>
+                <nav className='header-nav'>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">Product</a></li>
+                        <li><a href="#">Pages</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Buy Now</a></li>
+                    </ul>
+                </nav>
+                <div className='header-icons'>
+                    <div className='header-icon'>
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                    <div className='header-icon'>
+                        <i class="fa-regular fa-user"></i>
+                    </div>
+                    <div className='header-icon'>
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </div>
+                    <div className='header-icon'>
+                        <i class="fa-solid fa-bars"></i>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+    )
+}
+
+export default Header
